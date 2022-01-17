7896bf96123032de8988ee4ac037d10bc949b632
devcrvftwerk <devcrvft@gmail.com>
2022-01-17 02:35:40 -0500

    code cleanup

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import 'dotenv/config'
+import "dotenv/config";
 import fs from "fs-extra";
 import path from "path";
 import AdmZip from "adm-zip";
@@ -7,6 +7,8 @@ import puppeteer from "puppeteer";
 import simpleGit from "simple-git";
 import readline from "readline";
 import cliProgress from "cli-progress";
+import chalk from "chalk";
+import boxen from "boxen";
 const userEmail = process.env.USER_EMAIL;
 const userPass = process.env.USER_PASSWORD;
 const gitUsername = process.env.USER_NAME;
@@ -16,20 +18,18 @@ const gHubUserName = process.env.GHUB_USERNAME;
 const git = simpleGit();
 const width = 1200;
 const height = 900;
-import chalk from "chalk";
-import boxen from "boxen";
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 const greeting = chalk.white.bold("Webflow exports easy mode");
 
 const boxenOptions = {
- padding: 1,
- margin: 1,
- borderStyle: "round",
- borderColor: "green",
- backgroundColor: "#555555",
- title: chalk.blue.bold('Webflow Automation'),
- titleAlignment: 'center'
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+  title: chalk.blue.bold("Webflow Automation"),
+  titleAlignment: "center",
 };
 const msgBox = boxen(greeting, boxenOptions);
 
@@ -106,166 +106,170 @@ const autoweb = async () => {
     console.error(`This is the error: ${error}`);
   }
 
-  
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  rl.question(chalk.green.bold("Please enter the project name: "), async (project) => {
-    rl.question(chalk.green.bold("Please enter the commit message: "), async (commitMessage) => {
-      console.log("Thank You! 😃");
-      rl.close();
-      bar1.start(110, 0);
-      try {
-        await git.clone(
-          `https://${gitUsername}:${gitPAT}@github.com/${gitUsername}/${project}`,
-          `../download/clone`
-        );
-        git.cwd(`../download/clone`); //changes working directory.
-        bar1.update(10);
-      } catch (err) {
-        console.log(`nothing to clone because: ${err}`);
-      }
-
-      try {
-        let pages = await browser.pages(); // get all open pages by the browser
-        let popup = pages[pages.length - 1]; // the popup should be the last page opened
-
-        //this can be hard coded to speed up the whole process sans login.
-        await popup.keyboard.type(userEmail);
-        await popup.keyboard.press("Enter");
-        bar1.update(20);
-
-
-        popup = pages[pages.length - 1]; // the popup should be the last page opened. this is done again for the password box.
-
-        //this can be hard coded to speed up the whole process sans login.
-        setTimeout(async () => {
-          await popup.keyboard.type(userPass);
-          await popup.keyboard.press("Enter");
-        }, 4000);
-        bar1.update(30);
-
-
-        //waits for projects to load and opens user typed project.
-        try {
-          await page.waitForSelector(".hover-settings-container");
-
-          page.click(`div[site-name=${project}]`);
-          bar1.update(40);
-        } catch (error) {
-          console.error(error);
-        }
-        //clicks the export dropdown.
-        await page.waitForSelector(".bem-TopBar_Body_ExportButton");
-        //this setTime out length is because of the excessive time it takes to load the files in webflow. It will still sometimes fail to load ontime even with this.
-        setTimeout(async () => {
+  rl.question(
+    chalk.green.bold("Please enter the project name: "),
+    async (project) => {
+      rl.question(
+        chalk.green.bold("Please enter the commit message: "),
+        async (commitMessage) => {
+          console.log("Thank You! 😃");
+          rl.close();
+          bar1.start(110, 0);
           try {
-
-            await page.click(".bem-TopBar_Body_ExportButton");
-
-            bar1.update(50);
-          } catch (error) {
-            console.error(error);
-          }
-        }, 10000);
-        const targetPage = page;
-
-        //waits for and prepares download
-        const prepare = await waitForSelectors(
-          [
-            ["aria/Prepare ZIP", 'aria/[role="generic"]'],
-            [
-              "#designer-app-react-mount > div:nth-child(29) > div:nth-child(3) > div > div > div > div > div > div > div.kit-scrollbar > div:nth-child(9) > button:nth-child(4) > div",
-            ],
-          ],
-          targetPage
-        );
-        await prepare.click({ offset: { x: 0.875, y: -1.9000244140625 } });
-
-        bar1.update(60);
-
-        // clicks download.
-        const element = await waitForSelectors(
-          [
-            ["aria/Download ZIP", 'aria/[role="generic"]'],
-            [
-              "#designer-app-react-mount > div:nth-child(29) > div:nth-child(3) > div > div > div > div > div > div > div.kit-scrollbar > div:nth-child(9) > a > button > div:nth-child(2)",
-            ],
-          ],
-          targetPage
-        );
-        await element.click({
-          offset: { x: 27.2249755859375, y: 5.0999755859375 },
-        });
-
-        bar1.update(70);
-        //this setTimeout can be removed and replaced with something that reads file status----start file extraction/github flow..
-        setTimeout(async () => {
-          //
-          let fileNames = fs.readdirSync("../download/");
-          // chooses first file in the directory
-          const fileData = fs.readFileSync(`../download/${fileNames[1]}`);
-          //extracts download into the 'download/clone/${project}' dir.
-          const zip = new AdmZip(fileData);
-          await fs.emptyDir(`../download/clone/${project}`);//as of now the cloned project files are removed and the webflow download extracts into that folder. This makes the git flow easier.
-          zip.extractAllTo(
-            /*target path*/ `../download/clone/${project}`,
-            /*overwrite*/ true,
-          );
-          bar1.update(80)
-
-          fileNames = fs.readdirSync("../download/clone/");
-          try {
-            git.addConfig("user.email", `${gHubEmail}`);
-            git.addConfig("user.name", `${gHubUserName}`);
+            await git.clone(
+              `https://${gitUsername}:${gitPAT}@github.com/${gitUsername}/${project}`,
+              `../download/clone`
+            );
+            git.cwd(`../download/clone`); //changes working directory.
+            bar1.update(10);
           } catch (err) {
-            // Printing error if any occurs
-            console.error("error occured while " + "setting up git: " + err);
+            console.log(`nothing to clone because: ${err}`);
           }
 
-          // Add all files for commit
-          await git.add(`./${project}`).then(
-            (addSuccess) => {
-              console.log(addSuccess);
-              bar1.update(90)
-            },
-            (failedAdd) => {
-              console.log(`adding files failed ${failedAdd}`);
-            }
-          );
-          // Commit files
-          await git.commit(commitMessage).then(
-            (successCommit) => {
-              console.log(successCommit);
-              bar1.update(100)
-            },
-            (failed) => {
-              console.log(`failed commmit ${failed}`);
-            }
-          );
+          try {
+            let pages = await browser.pages(); // get all open pages by the browser
+            let popup = pages[pages.length - 1]; // the popup should be the last page opened
 
-          // Finally push to online repository.
-          await git.push("origin", "master").then(
-            (success) => {
-              bar1.update(110)
-              console.log(`repo successfully pushed: ${project}`);
-            },
-            (failed) => {
-              console.log(`repo push failed: ${failed}`);
-            }
-          );
+            //this can be hard coded to speed up the whole process sans login.
+            await popup.keyboard.type(userEmail);
+            await popup.keyboard.press("Enter");
+            bar1.update(20);
 
-          // this is used to clear the dir after git push.
-          await fs.emptyDir("../download");
-          console.log("delete success! finished!");
-          process.exit();
-        }, 7000);
-      } catch (error) {
-        console.error(`This is the error: ${error}`);
-      }
-    });
-  });
+            popup = pages[pages.length - 1]; // the popup should be the last page opened. this is done again for the password box.
+
+            //this can be hard coded to speed up the whole process sans login.
+            setTimeout(async () => {
+              await popup.keyboard.type(userPass);
+              await popup.keyboard.press("Enter");
+            }, 4000);
+            bar1.update(30);
+
+            //waits for projects to load and opens user typed project.
+            try {
+              await page.waitForSelector(".hover-settings-container");
+
+              page.click(`div[site-name=${project}]`);
+              bar1.update(40);
+            } catch (error) {
+              console.error(error);
+            }
+            //clicks the export dropdown.
+            await page.waitForSelector(".bem-TopBar_Body_ExportButton");
+            //this setTime out length is because of the excessive time it takes to load the files in webflow. It will still sometimes fail to load ontime even with this.
+            setTimeout(async () => {
+              try {
+                await page.click(".bem-TopBar_Body_ExportButton");
+
+                bar1.update(50);
+              } catch (error) {
+                console.error(error);
+              }
+            }, 10000);
+            const targetPage = page;
+
+            //waits for and prepares download
+            const prepare = await waitForSelectors(
+              [
+                ["aria/Prepare ZIP", 'aria/[role="generic"]'],
+                [
+                  "#designer-app-react-mount > div:nth-child(29) > div:nth-child(3) > div > div > div > div > div > div > div.kit-scrollbar > div:nth-child(9) > button:nth-child(4) > div",
+                ],
+              ],
+              targetPage
+            );
+            await prepare.click({ offset: { x: 0.875, y: -1.9000244140625 } });
+
+            bar1.update(60);
+
+            // clicks download.
+            const element = await waitForSelectors(
+              [
+                ["aria/Download ZIP", 'aria/[role="generic"]'],
+                [
+                  "#designer-app-react-mount > div:nth-child(29) > div:nth-child(3) > div > div > div > div > div > div > div.kit-scrollbar > div:nth-child(9) > a > button > div:nth-child(2)",
+                ],
+              ],
+              targetPage
+            );
+            await element.click({
+              offset: { x: 27.2249755859375, y: 5.0999755859375 },
+            });
+
+            bar1.update(70);
+            //this setTimeout can be removed and replaced with something that reads file status----start file extraction/github flow..
+            setTimeout(async () => {
+              //
+              let fileNames = fs.readdirSync("../download/");
+              // chooses first file in the directory
+              const fileData = fs.readFileSync(`../download/${fileNames[1]}`);
+              //extracts download into the 'download/clone/${project}' dir.
+              const zip = new AdmZip(fileData);
+              await fs.emptyDir(`../download/clone/${project}`); //as of now the cloned project files are removed and the webflow download extracts into that folder. This makes the git flow easier.
+              zip.extractAllTo(
+                /*target path*/ `../download/clone/${project}`,
+                /*overwrite*/ true
+              );
+              bar1.update(80);
+
+              fileNames = fs.readdirSync("../download/clone/");
+              try {
+                git.addConfig("user.email", `${gHubEmail}`);
+                git.addConfig("user.name", `${gHubUserName}`);
+              } catch (err) {
+                // Printing error if any occurs
+                console.error(
+                  "error occured while " + "setting up git: " + err
+                );
+              }
+
+              // Add all files for commit
+              await git.add(`./${project}`).then(
+                (addSuccess) => {
+                  console.log(addSuccess);
+                  bar1.update(90);
+                },
+                (failedAdd) => {
+                  console.log(`adding files failed ${failedAdd}`);
+                }
+              );
+              // Commit files
+              await git.commit(commitMessage).then(
+                (successCommit) => {
+                  console.log(successCommit);
+                  bar1.update(100);
+                },
+                (failed) => {
+                  console.log(`failed commmit ${failed}`);
+                }
+              );
+
+              // Finally push to online repository.
+              await git.push("origin", "master").then(
+                (success) => {
+                  bar1.update(110);
+                  console.log(`repo successfully pushed: ${project}`);
+                },
+                (failed) => {
+                  console.log(`repo push failed: ${failed}`);
+                }
+              );
+
+              // this is used to clear the dir after git push.
+              await fs.emptyDir("../download");
+              console.log("delete success! finished!");
+              process.exit();
+            }, 7000);
+          } catch (error) {
+            console.error(`This is the error: ${error}`);
+          }
+        }
+      );
+    }
+  );
 };
 autoweb();
